7fb28405557294b8f174de0170ed76f5c4c55e9b
David Duranty <dadaduranty@gmail.com>
2023-12-11 16:02:13 +0100

    add scroll filter update

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -4,20 +4,24 @@ function Filter() {
   return (
     <>
       <div className="container-filter">
-        <button type="button">Très facile</button>
-        <button type="button">Facile</button>
-        <button type="button">Moyen</button>
-        <button type="button">Difficile</button>
-        <button type="button">-30 minutes</button>
-        <button type="button">-1 heure</button>
+        <div className="scroll-bar">
+          <button type="button">Très facile</button>
+          <button type="button">Facile</button>
+          <button type="button">Moyen</button>
+          <button type="button">Difficile</button>
+          <button type="button">-30 minutes</button>
+          <button type="button">-1 heure</button>
+        </div>
       </div>
       <div className="container-filter">
-        <button type="button">Gourmand</button>
-        <button type="button">Santé</button>
-        <button type="button">Festif</button>
-        <button type="button">Sportif</button>
-        <button type="button">De saison</button>
-        <button type="button">BBQ</button>
+        <div className="scroll-bar">
+          <button type="button">Gourmand</button>
+          <button type="button">Santé</button>
+          <button type="button">Festif</button>
+          <button type="button">Sportif</button>
+          <button type="button">De saison</button>
+          <button type="button">BBQ</button>
+        </div>
       </div>
       <img className="boxFilter" src={blocFiltre} alt="Icon Filtre" />
     </>
