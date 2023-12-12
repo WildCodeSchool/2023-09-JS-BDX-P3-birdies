c24918468e0c888e9d77fef8be027df0e5a36c40
Victor G <victorgj95@outlook.com>
2023-12-12 17:22:53 +0100

    Login page structure done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,45 @@
+import cookies from "../styles/icons/cookies.jpg";
+import logo from "../styles/icons/logo.png";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <img className="cookies" src={cookies} alt="" />
+      <div className="img-container">
+        <img className="logo" src={logo} alt="" />
+      </div>
+      <div className="login-container">
+        <div className="header">
+          <div className="text">Connexion</div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <input type="text" placeholder="pseudo" />
+          </div>
+          <div className="input">
+            <input type="email" placeholder="email" />
+          </div>
+          <div className="input">
+            <input type="password" placeholder="mot de passe" />
+          </div>
+        </div>
+        <button type="submit" className="submit">
+          Se connecter
+        </button>
+        <div className="not-connected">
+          <div className="lost-password">
+            Mot de passe oublié? <br />
+            <span>Cliquez ici</span>
+          </div>
+          <div className="not-member">
+            Toujours pas membre? <br />
+            <span>S'enregistrer</span>
+          </div>
+        </div>
+        <p>Conditions générales d'utilisation</p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
