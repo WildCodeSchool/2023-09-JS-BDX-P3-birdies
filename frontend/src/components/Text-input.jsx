b4a103ae69f80d8ee781af37db4aae4ae62b9477
bfsylvain <sylvainbonnaure@hotmail.com>
2023-12-14 12:30:59 +0100

    Style update recipe page

---
+++ frontend/src/components/Text-input.jsx
@@ -0,0 +1,14 @@
+function TextInput() {
+  return (
+    <textarea
+      className="comment-input"
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      placeholder="Votre commentaire"
+    />
+  );
+}
+
+export default TextInput;
--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -1,169 +1,17 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import RecipeHeader from "../components/Recipe/RecipeHeader";
+import TextInput from "../components/Text-input";
+import ActionButton from "../components/action-button";
 import "../styles/Recipe.scss";
+import chevron from "../styles/icons/chevron-down 2.png";
+import lasagnes from "../styles/icons/lasagnes.jpg";
+import { Useinfo } from "../context/InfoContext";
+import CommentCard from "../components/CommentCard";
 
 function Recipe() {
-  const recipes = [
-    {
-      id: 1,
-      name: "Paella",
-      picture: "",
-      prepTime: "1h30",
-      notes: [3, 5, 2, 5, 4],
-      difficulty: "facile",
-      peopleNumber: 4,
-      ingredients: [
-        {
-          name: "riz",
-          quantity: 300,
-          mesure: "gr",
-        },
-        {
-          name: "poulet",
-          quantity: 200,
-          mesure: "gr",
-        },
-        {
-          name: "chorizo",
-          quantity: 100,
-          mesure: "gr",
-        },
-        {
-          name: "crevettes",
-          quantity: 100,
-          mesure: "gr",
-        },
-        {
-          name: "moules",
-          quantity: 80,
-          mesure: "gr",
-        },
-        {
-          name: "bouilon de volaille",
-          quantity: 300,
-          mesure: "gr",
-        },
-        {
-          name: "safran",
-          quantity: 0.1,
-          mesure: "gr",
-        },
-      ],
-      steps: [
-        {
-          description: "eplucher les légumes",
-        },
-        {
-          description: "cuire la viande",
-        },
-        {
-          description: "ajouter les fruits de mer",
-        },
-        {
-          description: "tout mélanger et c'est pret",
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: "Lasagnes",
-      picture: "",
-      prepTime: "2h00",
-      notes: [5, 5, 1, 3, 2, 4, 5],
-      difficulty: "Moyen",
-      peopleNumber: 6,
-      ingredients: [
-        {
-          name: "Pâte à lasagnes",
-          quantity: 500,
-          mesure: "gr",
-        },
-        {
-          name: "Tomates",
-          quantity: 1000,
-          mesure: "gr",
-        },
-        {
-          name: "viande hachée",
-          quantity: 1500,
-          mesure: "gr",
-        },
-        {
-          name: "Oignons",
-          quantity: 3,
-          mesure: "pce",
-        },
-        {
-          name: "lait",
-          quantity: 1000,
-          mesure: "ml",
-        },
-        {
-          name: "Beurre",
-          quantity: 200,
-          mesure: "gr",
-        },
-        {
-          name: "Farine",
-          quantity: 200,
-          mesure: "gr",
-        },
-        {
-          name: "Thym",
-          quantity: 3,
-          mesure: "branche",
-        },
-        {
-          name: "ail",
-          quantity: 3,
-          mesure: "gousse",
-        },
-      ],
-      steps: [
-        {
-          description: "cuire la viande, les oignons ensemble et l'ail",
-        },
-        {
-          description:
-            "Saler, poivrer et ajouter les tomates concassées. Faire mijoter 45mn",
-        },
-        {
-          description:
-            "Préparer la béchamel : Chauffer le beurre et la farine, ajouter le lait chaud tout en remuant",
-        },
-        {
-          description: "Sauce / Pate / Bechamel ===> repeat plein de fois",
-        },
-        {
-          description:
-            "rajouter le fromage on top qui n'est pas dans les ingrédients sa mère.",
-        },
-      ],
-    },
-  ];
-  const evaluations = [
-    {
-      image: "*",
-      word: "Bof",
-    },
-    {
-      image: "**",
-      word: "Passable",
-    },
-    {
-      image: "***",
-      word: "miam !",
-    },
-    {
-      image: "****",
-      word: "Delicious !",
-    },
-    {
-      image: "*****",
-      word: "Amaaazing !",
-    },
-  ];
+  const { recipes, evaluations, recipeNote, HandleRecipeNote } = Useinfo();
+
   function average(array) {
     const iniVal = 0;
     const NoteSum = array.reduce((acc, value) => value + acc, iniVal);
@@ -206,7 +54,7 @@ function Recipe() {
         <p>{chosenRecipe.name}</p>
       </div>
       <div className="recipe-img-container">
-        <img src="" alt="recipe-img" />
+        <img src={id !== 1 ? lasagnes : ""} alt="recipe-img" />
       </div>
 
       <div className="recipe-body-container">
@@ -250,7 +98,7 @@ function Recipe() {
           </div>
 
           {recipeQuantities.map((quantity) => (
-            <div className="ingredient-container">
+            <div key={quantity.name} className="ingredient-container">
               <div className="ingredient-name">{quantity.name}</div>
               <div className="ingred-qtty-mesure-container">
                 <div className="ingredient-qtty">
@@ -272,7 +120,7 @@ function Recipe() {
             </div>
           </div>
           {chosenRecipe.steps.map((step, index) => (
-            <div className="step-container">
+            <div key={step.index} className="step-container">
               <div className="step-title">Etape {index + 1}</div>
               <div className="step-text">
                 <p>{step.description}</p>
@@ -297,25 +145,36 @@ function Recipe() {
                 : "leave-comment-notes-area inactive"
             }
           >
-            {evaluations.map((evaluation) => (
-              <div className="notes-area">
-                <div className="stars-area">
-                  <p>{evaluation.image}</p>
-                </div>
-                <div>
-                  <p>{evaluation.word}</p>
-                </div>
-              </div>
-            ))}
+            <div className="all-notes-area">
+              {evaluations.map((evaluation) => (
+                <button
+                  key={evaluation.word}
+                  type="button"
+                  className={
+                    evaluation.word === recipeNote
+                      ? "notes-area clicked"
+                      : "notes-area"
+                  }
+                  onClick={HandleRecipeNote}
+                >
+                  <div className="stars-area">
+                    <p>{evaluation.image}</p>
+                  </div>
+                  <div>
+                    <p>{evaluation.word}</p>
+                  </div>
+                </button>
+              ))}
+            </div>
             <div className="comment-area">
               <p>Un petit commentaire à partager ?</p>
-              <input type="text" name="" id="" />
-              <button type="button">Publier mon avis</button>
+              <TextInput />
+              <ActionButton />
             </div>
           </div>
           <div className="open-close-btn">
-            <button type="button" onClick={toggleArea}>
-              X
+            <button className="chevron-btn" type="button" onClick={toggleArea}>
+              <img className="chevron" src={chevron} alt="chevron" />
             </button>
           </div>
           <div className="all-comments-area">
@@ -327,24 +186,17 @@ function Recipe() {
                 showComments ? "comments-bloc" : "comments-bloc inactive"
               }
             >
-              <div className="comment">
-                <div className="comment-header">
-                  <img src="" alt="user" />
-                  <p>Firstname Lastname</p>
-                  <div className="date-heure-area">date heure</div>
-                </div>
-              </div>
-              <div className="comment">
-                <div className="comment-header">
-                  <img src="" alt="user" />
-                  <p>Firstname Lastname</p>
-                  <div className="date-heure-area">date heure</div>
-                </div>
-              </div>
+              <CommentCard />
+              <CommentCard />
+              <CommentCard />
             </div>
             <div className="open-close-btn">
-              <button type="button" onClick={toggleArea2}>
-                X
+              <button
+                className="chevron-btn"
+                type="button"
+                onClick={toggleArea2}
+              >
+                <img className="chevron" src={chevron} alt="chevron" />
               </button>
             </div>
           </div>
