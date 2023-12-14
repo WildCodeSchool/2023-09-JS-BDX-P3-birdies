18e3cc93789adad39fd7ac9206bae3833a1baaf4
Victor G <victorgj95@outlook.com>
2023-12-14 16:50:05 +0100

    added functioning registration for new users and fixed scss

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
 import cookies from "../styles/icons/cookies.jpg";
 import logo from "../styles/icons/logo.png";
+import { Useinfo } from "../context/InfoContext";
 
 function Register() {
+  const { users, setUsers } = Useinfo();
+  const [pseudo, setPseudo] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [checkPassword, setCheckPassword] = useState();
+
+  const handleSubmit = () => {
+    if (password !== checkPassword) {
+      alert("Les mots de passe ne sont pas identiques"); // eslint-disable-line no-alert
+    } else {
+      const newUser = {
+        id: uuidv4(),
+        pseudo,
+        email,
+        password,
+      };
+      setUsers([...users, newUser]);
+      alert(`Bienvenu sur notre site, ${pseudo}!`); // eslint-disable-line no-alert
+    }
+  };
+
   return (
     <div className="container">
       <img className="cookies" src={cookies} alt="" />
-      <div className="img-container">
+      <div className="img-container img-container-registration">
         <img className="logo" src={logo} alt="" />
       </div>
       <div className="login-container">
@@ -15,32 +39,57 @@ function Register() {
             <div className="text">Créer mon compte</div>
           </div>
           <div className="input">
-            <input type="text" placeholder="Pseudo" />
+            <input
+              className="input-pseudo"
+              type="text"
+              placeholder="Pseudo"
+              onChange={(e) => setPseudo(e.target.value)}
+            />
           </div>
           <div className="input">
-            <input type="email" placeholder="Email" />
+            <input
+              className="input-email"
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="input">
-            <input type="password" placeholder="Mot de passe" />
+            <input
+              className="input-password"
+              type="password"
+              placeholder="Mot de passe"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="input">
-            <input type="password" placeholder="Vérifier mot de passe" />
+            <input
+              className="input-password"
+              type="password"
+              placeholder="Vérifier mot de passe"
+              onChange={(e) => setCheckPassword(e.target.value)}
+            />
           </div>
         </div>
         <div className="submit">
           <Link to="/login" className="link">
-            <button type="submit">S'enregistrer</button>
+            <button
+              className="submit-form"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              S'enregistrer
+            </button>
           </Link>
         </div>
 
-        <div className="not-connected">
+        <div className="not-connected not-connected-registration">
           <div className="not-member">
-            Oups, j'ai déjà un compte!
             <Link to="/login" className="link">
-              <span> Se connecter </span>
+              <span>Oups, j'ai déjà un compte!</span>
             </Link>
           </div>
-          <p>Conditions générales d'utilisation</p>
+          <p className="CGV">Conditions générales d'utilisation</p>
         </div>
       </div>
     </div>
