d46f8134ee0ac74419b19441328b1c3efd291f94
bfsylvain <sylvainbonnaure@hotmail.com>
2023-12-14 14:41:19 +0100

    mdb updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-
+import "./styles/app.scss";
 import App from "./App";
 import Login from "./pages/Login";
 import Recipe from "./pages/Recipe";
