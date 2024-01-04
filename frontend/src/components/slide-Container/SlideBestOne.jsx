a0008a3259405ee2a608adbf1e29a9df9402e350
David Duranty <dadaduranty@gmail.com>
2024-01-04 09:34:24 +0100

    Adjust sass home page

--- frontend/src/components/slide-Container/SlideBestOne.jsx
+++ frontend/src/components/slide-Container/SlideBestOne.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import { Useinfo } from "../../context/InfoContext";
+import star from "../../styles/icons/Star.png";
 
 function SlideBestOne() {
-  const { recipes } = Useinfo();
-  const [apiBoites, setApiBoites] = useState([]);
+  const { recipes, Average } = Useinfo();
+  // const [apiBoites, setApiBoites] = useState([]);
   const [likeBoites, setLikeBoites] = useState(new Map());
 
   async function toogleFavorite(name) {
     likeBoites.set(name, likeBoites.has(name) ? !likeBoites.get(name) : true); // fonction favorite
 
     setLikeBoites(likeBoites);
-    setApiBoites(apiBoites);
+    // setApiBoites(apiBoites);
   }
 
   const [reload, setReload] = useState(false);
@@ -25,20 +26,26 @@ function SlideBestOne() {
     <div className="slide-one-container">
       {recipes.map((recipe) => (
         <div className="card-container" key={recipe.id}>
-          <img src={recipe.picture} alt={recipe.name} />
           <div className="content-container">
             <h1>{recipe.name}</h1>
-            <p>{recipe.note}</p>
-            <div className="container-icon-recipe">
-              <button
-                className="btn-like-box"
-                type="button"
-                onClick={() => onLike(recipe.name)}
-              >
-                {likeBoites.get(recipe.name) ? "❤️" : "🤍"}
-              </button>
-              <span>{recipe.icon}</span>
+            <div className="note-container">
+              <img className="star-picture" src={star} alt="star-img" />
+              <div className="average">{Average(recipe.notes)}/5</div>
             </div>
+            <div className="votes-container">
+              <p id="votes-content">{recipe.notes.length} votes</p>
+            </div>
+          </div>
+          <img src={recipe.picture} alt={recipe.name} />
+          <div className="container-icon-recipe">
+            <button
+              className="btn-like-box"
+              type="button"
+              onClick={() => onLike(recipe.name)}
+            >
+              {likeBoites.get(recipe.name) ? "❤️" : "🤍"}
+            </button>
+            <span>{recipe.icon}</span>
           </div>
         </div>
       ))}
--- frontend/src/components/slide-Container/SlideBestTwo.jsx
+++ frontend/src/components/slide-Container/SlideBestTwo.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Useinfo } from "../../context/InfoContext";
+import star from "../../styles/icons/Star.png";
 
 function SlideBestTwo() {
-  const { recipesPepites } = Useinfo();
+  const { recipesPepites, Average } = Useinfo();
   const [apiBoites, setApiBoites] = useState([]);
   const [likeBoites, setLikeBoites] = useState(new Map());
 
@@ -24,20 +25,26 @@ function SlideBestTwo() {
     <div className="slide-one-container">
       {recipesPepites.map((recipe) => (
         <div className="card-container" key={recipe.id}>
-          <img src={recipe.picture} alt={recipe.name} />
           <div className="content-container">
             <h1>{recipe.name}</h1>
-            <p>{recipe.note}</p>
-            <div className="container-icon-recipe">
-              <button
-                className="btn-like-box"
-                type="button"
-                onClick={() => onLike(recipe.name)}
-              >
-                {likeBoites.get(recipe.name) ? "❤️" : "🤍"}
-              </button>
-              <span>{recipe.icon}</span>
+            <div className="note-container">
+              <img className="star-picture" src={star} alt="star-img" />
+              <div className="average">{Average(recipe.notes)}/5</div>
             </div>
+            <div className="votes-container">
+              <p id="votes-content">{recipe.notes.length} votes</p>
+            </div>
+          </div>
+          <img src={recipe.picture} alt={recipe.name} />
+          <div className="container-icon-recipe">
+            <button
+              className="btn-like-box"
+              type="button"
+              onClick={() => onLike(recipe.name)}
+            >
+              {likeBoites.get(recipe.name) ? "❤️" : "🤍"}
+            </button>
+            <span>{recipe.icon}</span>
           </div>
         </div>
       ))}
