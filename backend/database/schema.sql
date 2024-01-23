cc01136291d61ad38685f1372b9c6477bc5ca390
bfsylvain <sylvainbonnaure@hotmail.com>
2024-01-23 10:29:33 +0100

    format sql file

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,16 +1,14 @@
 -- SQLBook: Code
 -- SQLBook: Code
-
-create table
-    `users` (
-        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        `firstname` VARCHAR(255),
-        `lastname` VARCHAR(255),
-        `pseudo` VARCHAR(255) UNIQUE,
-        `email` VARCHAR(255) UNIQUE NOT NULL,
-        `password` VARCHAR(255) NOT NULL,
-        `role` VARCHAR(255) NOT NULL
-    );
+create table `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `firstname` VARCHAR(255),
+    `lastname` VARCHAR(255),
+    `pseudo` VARCHAR(255) UNIQUE,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `role` VARCHAR(255) NOT NULL
+);
 
 INSERT INTO
     `users` (
@@ -21,14 +19,16 @@ INSERT INTO
         `password`,
         `role`
     )
-VALUES (
+VALUES
+    (
         "John",
         "Wick",
         "johnnyBoy",
         "johnwick@wanadoo.fr",
         "123456",
         "admin"
-    ), (
+    ),
+    (
         "Patata",
         "Doe",
         "tikaMassala",
@@ -37,19 +37,19 @@ VALUES (
         "user"
     );
 
-create table
-    `recipes` (
-        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        `userId` INT NOT NULL,
-        `name` VARCHAR(255) NOT NULL,
-        `publicationDate` VARCHAR(255) NOT NULL,
-        `picture` VARCHAR(255) NOT NULL,
-        `peopleNumber` INT NOT NULL,
-        `energyPerPerson` INT NOT NULL,
-        `difficulty` VARCHAR(255) NOT NULL,
-        `prepTime` INT not NULL
-    );
-    drop TABLE recipes;
+create table `recipes` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `userId` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `publicationDate` VARCHAR(255) NOT NULL,
+    `picture` VARCHAR(255) NOT NULL,
+    `peopleNumber` INT NOT NULL,
+    `energyPerPerson` INT NOT NULL,
+    `difficulty` VARCHAR(255) NOT NULL,
+    `prepTime` INT not NULL
+);
+
+drop TABLE recipes;
 
 CREATE table `upload` (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -57,69 +57,78 @@ CREATE table `upload` (
     UNIQUE(url),
     created_at TIMESTAMP default CURRENT_TIMESTAMP
 );
-alter TABLE `recipes` MODIFY picture INT, ADD CONSTRAINT fk_picture_upload_id FOREIGN KEY (picture) REFERENCES upload(id);
 
-CREATE table
-    `ingredients` (
-        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        `ingredientName` VARCHAR(255) NOT NULL
-    );
+alter TABLE
+    `recipes`
+MODIFY
+    picture INT,
+ADD
+    CONSTRAINT fk_picture_upload_id FOREIGN KEY (picture) REFERENCES upload(id);
 
-CREATE TABLE
-    `recipes_ingredients` (
-        `recipe_id` INT NOT NULL,
-        `ingredient_id` INT NOT NULL,
-        `unite` VARCHAR(255) NOT NULL,
-        `quantity` INT NOT NULL,
-        Foreign Key (`recipe_id`) REFERENCES recipes(id),
-        Foreign Key (`ingredient_id`) REFERENCES ingredients(id)
-    );
+CREATE table `ingredients` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `ingredientName` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `recipes_ingredients` (
+    `recipe_id` INT NOT NULL,
+    `ingredient_id` INT NOT NULL,
+    `unite` VARCHAR(255) NOT NULL,
+    `quantity` INT NOT NULL,
+    Foreign Key (`recipe_id`) REFERENCES recipes(id),
+    Foreign Key (`ingredient_id`) REFERENCES ingredients(id)
+);
+
 drop table `recipes_ingredients`;
-CREATE TABLE
-    `cathegories` (
-        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        `name` VARCHAR(255) NOT NULL
-    );
 
-CREATE TABLE
-    `recipes_cathegories` (
-        `recipe_id` INT NOT NULL,
-        `cathegory_id` INT NOT NULL,
-        Foreign Key (`recipe_id`) REFERENCES recipes(id),
-        Foreign Key (`cathegory_id`) REFERENCES cathegories(id)
-    );
+CREATE TABLE `cathegories` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `recipes_cathegories` (
+    `recipe_id` INT NOT NULL,
+    `cathegory_id` INT NOT NULL,
+    Foreign Key (`recipe_id`) REFERENCES recipes(id),
+    Foreign Key (`cathegory_id`) REFERENCES cathegories(id)
+);
+
 drop TABLE `recipes_cathegories`;
-CREATE TABLE
-    `favorite_Recipes` (
-        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        `user_id` INT NOT NULL,
-        `recipe_id` INT NOT NULL,
-        Foreign Key (user_id) REFERENCES users(id),
-        Foreign Key (recipe_id) REFERENCES recipes(id)
-    );
+
+CREATE TABLE `favorite_Recipes` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `recipe_id` INT NOT NULL,
+    Foreign Key (user_id) REFERENCES users(id),
+    Foreign Key (recipe_id) REFERENCES recipes(id)
+);
+
 drop table `favorite_Recipes`;
-CREATE table 
-    `evaluations` (
-        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        `user_id` INT NOT NULL,
-        `recipe_id` INT NOT NULL,
-        `comment` VARCHAR(255),
-        `note` INT NOT NULL,
-        `commentDate` timestamp default CURRENT_TIMESTAMP,
-        `transformedDate` VARCHAR(255),
-        Foreign Key (user_id) REFERENCES users(id),
-        Foreign Key (recipe_id) REFERENCES recipes(id)
-    );
+
+CREATE table `evaluations` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `recipe_id` INT NOT NULL,
+    `comment` VARCHAR(255),
+    `note` INT NOT NULL,
+    `commentDate` timestamp default CURRENT_TIMESTAMP,
+    `transformedDate` VARCHAR(255),
+    Foreign Key (user_id) REFERENCES users(id),
+    Foreign Key (recipe_id) REFERENCES recipes(id)
+);
+
 drop table `evaluations`;
-CREATE TABLE
-    `steps` (
-        `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        `recipe_id` INT NOT NULL,
-        `description` VARCHAR(500) NOT NULL,
-        `position` INT NOT NULL,
-        Foreign Key (recipe_id) REFERENCES recipes(id)
-    );
+
+CREATE TABLE `steps` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `recipe_id` INT NOT NULL,
+    `description` VARCHAR(500) NOT NULL,
+    `position` INT NOT NULL,
+    Foreign Key (recipe_id) REFERENCES recipes(id)
+);
+
 drop table `steps`;
+
 INSERT INTO
     `recipes` (
         `userId`,
@@ -131,7 +140,8 @@ INSERT INTO
         `difficulty`,
         `prepTime`
     )
-VALUES (
+VALUES
+    (
         "1",
         "Pâtes carbonara",
         "08/01/2024",
@@ -140,7 +150,8 @@ VALUES (
         "188",
         "facile",
         "30"
-    ), (
+    ),
+    (
         "2",
         "Pâtes bolognaise",
         "08/01/2024",
@@ -149,7 +160,8 @@ VALUES (
         "150",
         "facile",
         "90"
-    ), (
+    ),
+    (
         "3",
         "Pâtes au saumon",
         "08/01/2024",
@@ -159,6 +171,7 @@ VALUES (
         "facile",
         "25"
     );
+
 INSERT INTO
     `recipes` (
         `userId`,
@@ -170,7 +183,8 @@ INSERT INTO
         `difficulty`,
         `prepTime`
     )
-VALUES (
+VALUES
+    (
         "4",
         "Pâtes au pesto",
         "10/01/2024",
@@ -181,47 +195,113 @@ VALUES (
         "45"
     );
 
-INSERT INTO `cathegories`(`name`) VALUES ("Très facile");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Facile");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Moyen");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Difficile");
-
-INSERT INTO `cathegories`(`name`) VALUES ("-30 minutes");
-
-INSERT INTO `cathegories`(`name`) VALUES ("-1 heure");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Gourmand");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Festif");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Sportif");
-
-INSERT INTO `cathegories`(`name`) VALUES ("De saison");
-
-INSERT INTO `cathegories`(`name`) VALUES ("BBQ");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Végétarien");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Sans lactose");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Végan");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Sans gluten");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Entrée");
-
-INSERT INTO `cathegories`(`name`) VALUES ("Snack"), ("Plat");
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Très facile");
 
 INSERT INTO
     `cathegories`(`name`)
-VALUES ("Dessert"), ("Boissons"), ("Français"), ("Asiatique"), ("Méditerranéen"), ("Oriental"), ("Caribéen"), ("Hispanique"), ("USA"), ("Québecois");
+VALUES
+    ("Facile");
 
-ALTER TABLE `recipes` ADD `Gender` VARCHAR(255);
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Moyen");
 
-ALTER TABLE `recipes` DROP `Gender`;
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Difficile");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("-30 minutes");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("-1 heure");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Gourmand");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Festif");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Sportif");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("De saison");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("BBQ");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Végétarien");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Sans lactose");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Végan");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Sans gluten");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Entrée");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Snack"),
+    ("Plat");
+
+INSERT INTO
+    `cathegories`(`name`)
+VALUES
+    ("Dessert"),
+    ("Boissons"),
+    ("Français"),
+    ("Asiatique"),
+    ("Méditerranéen"),
+    ("Oriental"),
+    ("Caribéen"),
+    ("Hispanique"),
+    ("USA"),
+    ("Québecois");
+
+ALTER TABLE
+    `recipes`
+ADD
+    `Gender` VARCHAR(255);
+
+ALTER TABLE
+    `recipes` DROP `Gender`;
 
 INSERT INTO
     `recipes` (
@@ -234,7 +314,8 @@ INSERT INTO
         `difficulty`,
         `prepTime`
     )
-VALUES (
+VALUES
+    (
         "5",
         "Croziflette",
         "11/01/2024",
@@ -243,7 +324,8 @@ VALUES (
         "240",
         "Facile",
         "50"
-    ), (
+    ),
+    (
         "6",
         "Recette soufflé au Grand Marnier",
         "11/01/2024",
@@ -252,7 +334,8 @@ VALUES (
         "120",
         "Difficile",
         "40"
-    ), (
+    ),
+    (
         "7",
         " Recette pastilla de pigeon aux raisins blancs",
         "11/01/2024",
@@ -262,5 +345,3 @@ VALUES (
         "Difficile",
         "45"
     );
-
-    
