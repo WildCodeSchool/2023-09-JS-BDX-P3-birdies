faabc21ea382fe1c997b8a40a84491302f210fa4
Victor G <victorgj95@outlook.com>
2024-03-20 16:40:00 +0100

    added comments for better understanding

--- backend/src/models/uploadManager.js
+++ backend/src/models/uploadManager.js
@@ -9,19 +9,25 @@ class UploadManager extends AbstractManager {
 
   create(data) {
     console.info(data);
+    // data.destination et data.filename sont combinés pour créer data.originalname, split et slice pour enlever le suffixe ".jpg" ou autre
     const filename = `${data.destination}${data.filename}.${data.originalname
       .split(".")
       .slice(-1)}`;
     // on utilise le path car dans le .path plutot que .destination il y a des \ qui sont necessaire si mac
+    // on fais une promesse pour gérer le temps d'exécution et rien bloquer
     return new Promise((resolve, reject) => {
+      // fs.rename est une fonction node.js pour déplacer le fichier data.path dans filename
+      // si erreur tout s'arrête
       fs.rename(data.path, filename, async (err) => {
         if (err) {
           reject(err);
         }
+        // sinon requête sql pour insérer url dans upload et on enlève "public/" dans le filename
         const [result] = await this.database.query(
           `insert into ${this.table} (url) values (?)`,
           [filename.replace("public/", "")]
         );
+        // on résout la promesse en insérant un id au hasard et filename dans la colonne url
         console.info(result);
         resolve({
           id: result.insertId,
